168cd021fadcf18de4f09e3f53499d2b18868d4c
Slail Billicko Jean Pierre <sbillicko@gmail.com>
2021-10-22 18:53:36 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+def tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest):
+    """
+    Write a function that returns the maximum possible total speed or the minimum possible total speed of all of the
+    tandem bicycles being ridden based on an input parameter, fastest .If fastest = true your function should return the
+    maximum possible total speed; otherwise it should return the minimum total speed.
+    """
+    pass
