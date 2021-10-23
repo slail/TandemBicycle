9abb0eb9821b4910ac1c3d5f1907a6a2838eaddb
Slail Billicko Jean Pierre <sbillicko@gmail.com>
2021-10-23 12:27:47 -0400

    added algorithm

--- main.py
+++ main.py
@@ -4,4 +4,24 @@ def tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest):
     tandem bicycles being ridden based on an input parameter, fastest .If fastest = true your function should return the
     maximum possible total speed; otherwise it should return the minimum total speed.
     """
-    pass
+    redShirtSpeeds.sort()
+    blueShirtSpeeds.sort()
+
+    if not fastest:
+        reverseArray(redShirtSpeeds)
+
+    total = 0
+    for idx in range(len(redShirtSpeeds)):
+        rider1 = redShirtSpeeds[idx]
+        rider2 = blueShirtSpeeds[len(blueShirtSpeeds) - 1 - idx]
+        total += max(rider1, rider2)
+    return total
+
+
+def reverseArray(array):
+    start = 0
+    end = len(array) - 1
+    while start < end:
+        array[start], array[end] = array[end], array[start]
+        start += 1
+        end -= 1
